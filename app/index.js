9cb3921301ad311dc7d0ee05a766f09f7ed02ad0
felix <felix@volny.co>
2016-08-31 14:30:24 +0700

    append table rows with recursive func

--- app/index.js
+++ app/index.js
@@ -4,25 +4,20 @@ import $ from 'jquery';
 
 const streamers = ["brunofin","ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
 
-function makeURL(streamer, type) {
-  return 'https://api.twitch.tv/kraken/' + type  + '/' + streamer + '?callback=?';
+function makeURL(streamer) {
+  return 'https://api.twitch.tv/kraken/streams/' + streamer + '?callback=?';
 }
 
-// returns a deferred object
-function getData(url) {
+// returns a promise
+function getJSONP(url) {
   return $.getJSON(url, {
     format: "jsonp"
   });
 }
 
-// returns a deferred object
-function getChannelURL(streamer) {
-  return getData(makeURL(streamer, 'streams'));
-}
-
-// returns an array of arrays containing streamer name and deferred object
-function getAllChannels(streamers) {
-  return streamers.map((streamer) => ([streamer, getChannelURL(streamer)]))
+// returns a promise
+function getData(streamer) {
+  return getJSONP(makeURL(streamer));
 }
 
 function getStreamerStatus(obj) {
@@ -38,20 +33,23 @@ function getStreamerStatus(obj) {
   }
 }
 
-// instead of logging it put in in a table in the DOM
-function insertData(data) {
-  $('tbody').append('<tr><td>Streamer</td><td>' + getStreamerStatus(data) + '</td></tr>')
+// forget about the <a> - the only way to make tablerows clickable is with an eventhandler
+function makeRowString(streamer, status, url) {
+  return '<tr><td>' + streamer + '</td><td>' + status + '</td></tr>';
 }
 
-const logStatus = (data) => console.log(getStreamerStatus(data))
-
-getAllChannels(streamers).map((array) => {
-    //array[1].done(logStatus);
-    array[1].done(insertData);
+function appendRows(streamers) {
+  if (streamers.length === 0) {
+    return false;
   }
-);
 
-// todo
-// get the names in the table
-// make alternating rows striped
+  getData(streamers[0]).done((data) => {
+    let rowString = makeRowString(streamers[0], getStreamerStatus(data), 'https://google.com');
+    $('tbody').append(rowString);
+  });
+
+  return appendRows(streamers.slice(1));
+}
+
+appendRows(streamers);
 
